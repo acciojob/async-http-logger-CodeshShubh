89f70cd08514cc5afb035ad394e8db3097b63e5e
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2025-01-22 19:22:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ let url = "https://jsonplaceholder.typicode.com/todos/1"
 async function fetchApi() {
 	let response = await fetch(url);
 	let data = await response.json();
-	console.log(data);
 	return data
 	
 }
 
-
-console.log(fetchApi());
+fetchApi().then(data => console.log(data))
