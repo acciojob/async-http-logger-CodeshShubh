4fa6d8dcc25bc17284f3bbbf3d387f23b8b8fbdd
Shubhanshu Saadhiyaan <138874847+CodeshShubh@users.noreply.github.com>
2025-01-22 19:20:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let url = "https://jsonplaceholder.typicode.com/todos/1"
+async function fetchApi() {
+	let response = await fetch(url);
+	let data = await response.json();
+	console.log(data);
+	return data
+	
+}
+
+
+console.log(fetchApi());
